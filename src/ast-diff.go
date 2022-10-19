5892ed26085397e6e04b96bb809c673f90e66010
David Mertz <mertz@gnosis.cx>
2022-10-19 15:43:05 -0400

    'Does something' golang version

--- src/ast-diff.go
+++ src/ast-diff.go
@@ -11,11 +11,14 @@ used with those languages.
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
+	"github.com/fatih/color"
 	"log"
 	"os"
 	"os/exec"
+	"strings"
 )
 
 const usage = `Usage of ast-dff:
@@ -23,7 +26,54 @@ const usage = `Usage of ast-dff:
   -l, --semantic List semantically meaningful changes since last git commit
   -g, --glob     Limit compared files by a glob pattern
   -v, --verbose  Show verbose output on STDERR
+  -h, --help     Display this help screen
 `
+
+type GitStatus int8
+
+const (
+	Preamble GitStatus = iota
+	Staged
+	Unstaged
+	Untracked
+)
+
+func ParseGitStatus(status []byte, semantic bool) {
+	lines := bytes.Split(status, []byte("\n"))
+	var section GitStatus = Preamble
+	header := color.New(color.FgWhite, color.Bold)
+	untracked := color.New(color.FgRed).Add(color.Underline)
+	for i := 0; i < len(lines); i++ {
+		line := string(lines[i])
+		if strings.HasPrefix(line, "Changes to be committed") {
+			section = Staged
+			header.Println(line)
+		} else if strings.HasPrefix(line, "Changes not staged for commit") {
+			section = Unstaged
+			header.Println(line)
+		} else if strings.HasPrefix(line, "Untracked files") {
+			section = Untracked
+			header.Println(line)
+		}
+
+		if strings.HasPrefix(line, "\t") {
+			switch section {
+			case Staged:
+				color.Green(line)
+				if semantic {
+					color.Cyan("... Actual AST comparison here ...")
+				}
+			case Unstaged:
+				color.Red(line)
+				if semantic {
+					color.Cyan("... Actual AST comparison here ...")
+				}
+			case Untracked:
+				untracked.Println(line)
+			}
+		}
+	}
+}
 
 func main() {
 	var status bool
@@ -38,9 +88,9 @@ func main() {
 	flag.StringVar(&glob, "glob", "*", "Limit compared files by a glob pattern")
 	flag.StringVar(&glob, "g", "*", "Limit compared files by glob (short flag)")
 
-    var verbose bool
-    flag.BoolVar(&verbose, "verbose", false, "Show verbose output on STDERR")
-    flag.BoolVar(&verbose, "v", false, "Show verbose output on STDERR")
+	var verbose bool
+	flag.BoolVar(&verbose, "verbose", false, "Show verbose output on STDERR")
+	flag.BoolVar(&verbose, "v", false, "Show verbose output on STDERR")
 
 	flag.Usage = func() { fmt.Print(usage) }
 	flag.Parse()
@@ -53,15 +103,12 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
+		ParseGitStatus(out, semantic)
 	}
 
-	if semantic {
-		// Do the actual semantic diff
+	if verbose {
+		fmt.Fprintf(os.Stderr, "status: %t\n", status)
+		fmt.Fprintf(os.Stderr, "semantic: %t\n", semantic)
+		fmt.Fprintf(os.Stderr, "glob: %s\n", glob)
 	}
-
-	fmt.Fprintf(os.Stdout, "%s\n", out)
-
-	fmt.Fprintf(os.Stderr, "status: %t\n", status)
-	fmt.Fprintf(os.Stderr, "semantic: %t\n", semantic)
-	fmt.Fprintf(os.Stderr, "glob: %s\n", glob)
 }
