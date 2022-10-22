f1c309c3f4dff73279815c75dcc247b27d7e931c
David Mertz <mertz@gnosis.cx>
2022-10-22 18:48:49 -0400

    Restore reference version of Ruby script

--- samples/simple-filter.rb
+++ samples/simple-filter.rb
@@ -1,10 +1,7 @@
 #!/usr/bin/env ruby
 def mod5?(items)
-  items.to_a.select {
-    |item| item % 5 == 0
-  }
+  items.to_a.select {|item| item % 5 == 0}
 end
 
-puts mod5? 1..50
-puts mod5? 1..101
+puts mod5? 1..100
 
