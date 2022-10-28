6668f2a23fd516660bf014fe79170f4a959724ec
David Mertz <mertz@gnosis.cx>
2022-10-28 09:28:54 -0400

    Cleanup pass with gofmt

--- pkg/sql/sql.go
+++ pkg/sql/sql.go
@@ -1,113 +1,113 @@
 package sql
 
 import (
-    "bytes"
-    "fmt"
-    "io/ioutil"
-    "log"
-    "os"
-    "os/exec"
-    "regexp"
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"regexp"
 
-    "github.com/sergi/go-diff/diffmatchpatch"
+	"github.com/sergi/go-diff/diffmatchpatch"
 
-    "github.com/atlantistechnology/sdt/pkg/types"
-    "github.com/atlantistechnology/sdt/pkg/utils"
+	"github.com/atlantistechnology/sdt/pkg/types"
+	"github.com/atlantistechnology/sdt/pkg/utils"
 )
 
 // colorDiff converts (DiffMatchPatch, []Diff) into colored text report
 func colorDiff(
-    dmp *diffmatchpatch.DiffMatchPatch,
-    diffs []diffmatchpatch.Diff) string {
-    var buff bytes.Buffer
-    // Tool `sqlformat` doesn't normalize whitespace completely
-    reWhiteSpace := regexp.MustCompile("^[\n\r\t ]+$")
+	dmp *diffmatchpatch.DiffMatchPatch,
+	diffs []diffmatchpatch.Diff) string {
+	var buff bytes.Buffer
+	// Tool `sqlformat` doesn't normalize whitespace completely
+	reWhiteSpace := regexp.MustCompile("^[\n\r\t ]+$")
 
-    _, _ = buff.WriteString(
-        "\x1b[33mComparison of canonicalized SQL (HEAD -> Current)\x1b[0m\n",
-    )
+	_, _ = buff.WriteString(
+		"\x1b[33mComparison of canonicalized SQL (HEAD -> Current)\x1b[0m\n",
+	)
 
-    changed := false
-    for _, diff := range diffs {
-        text := diff.Text
+	changed := false
+	for _, diff := range diffs {
+		text := diff.Text
 
-        switch diff.Type {
-        case diffmatchpatch.DiffInsert:
-            if !reWhiteSpace.MatchString(text) {
-                changed = true
-            }
-            _, _ = buff.WriteString("\x1b[32m")
-            _, _ = buff.WriteString(text)
-            _, _ = buff.WriteString("\x1b[0m")
-        case diffmatchpatch.DiffDelete:
-            if !reWhiteSpace.MatchString(text) {
-                changed = true
-            }
-            _, _ = buff.WriteString("\x1b[31m")
-            _, _ = buff.WriteString(text)
-            _, _ = buff.WriteString("\x1b[0m")
-        case diffmatchpatch.DiffEqual:
-            _, _ = buff.WriteString("\x1b[0m")
-            _, _ = buff.WriteString(text)
-        }
-    }
-    if changed {
-        return utils.BufferToDiff(buff, true)
-    }
-    return "| No semantic differences detected"
+		switch diff.Type {
+		case diffmatchpatch.DiffInsert:
+			if !reWhiteSpace.MatchString(text) {
+				changed = true
+			}
+			_, _ = buff.WriteString("\x1b[32m")
+			_, _ = buff.WriteString(text)
+			_, _ = buff.WriteString("\x1b[0m")
+		case diffmatchpatch.DiffDelete:
+			if !reWhiteSpace.MatchString(text) {
+				changed = true
+			}
+			_, _ = buff.WriteString("\x1b[31m")
+			_, _ = buff.WriteString(text)
+			_, _ = buff.WriteString("\x1b[0m")
+		case diffmatchpatch.DiffEqual:
+			_, _ = buff.WriteString("\x1b[0m")
+			_, _ = buff.WriteString(text)
+		}
+	}
+	if changed {
+		return utils.BufferToDiff(buff, true)
+	}
+	return "| No semantic differences detected"
 }
 
 func Diff(filename string, options types.Options, config types.Config) string {
-    var currentCanonical []byte
-    var head []byte
-    var headCanonical []byte
-    var err error
-    sqlCmd := config.Commands["sql"].Executable
-    switches := append(config.Commands["sql"].Switches, filename)
+	var currentCanonical []byte
+	var head []byte
+	var headCanonical []byte
+	var err error
+	sqlCmd := config.Commands["sql"].Executable
+	switches := append(config.Commands["sql"].Switches, filename)
 
-    // Get the AST for the current version of the file
-    cmdCurrentCanonical := exec.Command(sqlCmd, switches...)
-    currentCanonical, err = cmdCurrentCanonical.Output()
-    if err != nil {
-        log.Fatal(err)
-    }
+	// Get the AST for the current version of the file
+	cmdCurrentCanonical := exec.Command(sqlCmd, switches...)
+	currentCanonical, err = cmdCurrentCanonical.Output()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    // Retrieve the HEAD version of the file to a temporary filename
-    cmdHead := exec.Command("git", "show", fmt.Sprintf("HEAD:%s", filename))
-    head, err = cmdHead.Output()
-    if err != nil {
-        log.Fatal(err)
-    }
+	// Retrieve the HEAD version of the file to a temporary filename
+	cmdHead := exec.Command("git", "show", fmt.Sprintf("HEAD:%s", filename))
+	head, err = cmdHead.Output()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    tmpfile, err := ioutil.TempFile("", "*.sql")
-    if err != nil {
-        log.Fatal(err)
-    }
-    tmpfile.Write(head)
-    defer os.Remove(tmpfile.Name()) // clean up
+	tmpfile, err := ioutil.TempFile("", "*.sql")
+	if err != nil {
+		log.Fatal(err)
+	}
+	tmpfile.Write(head)
+	defer os.Remove(tmpfile.Name()) // clean up
 
-    // Get the AST for the HEAD version of the file
-    switches = append(config.Commands["sql"].Switches, tmpfile.Name())
-    cmdHeadCanonical := exec.Command(sqlCmd, switches...)
-    headCanonical, err = cmdHeadCanonical.Output()
-    if err != nil {
-        log.Fatal(err)
-    }
+	// Get the AST for the HEAD version of the file
+	switches = append(config.Commands["sql"].Switches, tmpfile.Name())
+	cmdHeadCanonical := exec.Command(sqlCmd, switches...)
+	headCanonical, err = cmdHeadCanonical.Output()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    // Perform the diff between the versions
-    // Our canonicalizer isn't always consistent with trailing spaces
-    a := string(headCanonical)
-    b := string(currentCanonical)
-    dmp := diffmatchpatch.New()
-    diffs := dmp.DiffMain(a, b, false)
+	// Perform the diff between the versions
+	// Our canonicalizer isn't always consistent with trailing spaces
+	a := string(headCanonical)
+	b := string(currentCanonical)
+	dmp := diffmatchpatch.New()
+	diffs := dmp.DiffMain(a, b, false)
 
-    if options.Parsetree {
-        return "| SQL comparison uses canonicalization not AST analysis"
-    }
+	if options.Parsetree {
+		return "| SQL comparison uses canonicalization not AST analysis"
+	}
 
-    if options.Semantic {
-        return colorDiff(dmp, diffs)
-    }
+	if options.Semantic {
+		return colorDiff(dmp, diffs)
+	}
 
-    return "| No diff type specified"
+	return "| No diff type specified"
 }
