3aed55a3184ab670625409552749a5e0923f8031
David Mertz <mertz@gnosis.cx>
2022-10-31 16:39:38 -0400

    Add Python sample with semantic differences

---
+++ samples/funcs2.py
@@ -0,0 +1,40 @@
+def add(a, b):
+    "Add two numbers together"
+    total = b+a
+    return total
+
+
+def pow(a, b):
+    "Take one number to power of another"
+    power = a**b
+    return power
+
+
+def mul(a, b):
+    "Multiply two numbers together"
+    product = a * b
+    return product
+
+
+def div(a, b):
+    "Divide a number by another number"
+    ratio = a//b
+    return ratio
+
+
+def less(a, b):
+    "Find the lesser of two numbers"
+    small = min(a, b)
+    return small
+
+
+def more(a, b):
+    "Find the greater of two numbers"
+    big = max(a, b)
+    return big
+
+
+def sub(a, b):
+    "Subtract a number from another number"
+    diff = a-b
+    return diff
