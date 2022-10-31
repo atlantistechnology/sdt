844a4d57109dd397bfbf312a099f83d3ec016aa7
David Mertz <mertz@gnosis.cx>
2022-10-31 16:32:01 -0400

    Trying to force discontinuous diff segments

--- samples/3func.py
+++ samples/3func.py
@@ -4,6 +4,12 @@ def add(a, b):
     return total
 
 
+def pow(a, b):
+    "Take one number to power of another"
+    power = a**b
+    return power
+
+
 def mul(a, b):
     "Multiply two numbers together"
     product = a * b
@@ -14,3 +20,21 @@ def div(a, b):
     "Divide a number by another number"
     ratio = a / b
     return ratio
+
+
+def less(a, b):
+    "Find the lesser of two numbers"
+    small = min(a, b)
+    return small
+
+
+def more(a, b):
+    "Find the greater of two numbers"
+    big = max(a, b)
+    return big
+
+
+def sub(a, b):
+    "Subtract a number from another number"
+    diff = a - b
+    return diff
