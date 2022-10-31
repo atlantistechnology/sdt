bb1ba62da1b37e5b37d4f2f9047a44d1089009e3
David Mertz <mertz@gnosis.cx>
2022-10-31 16:16:42 -0400

    Add Python sample files

---
+++ samples/3func1.py
@@ -0,0 +1,16 @@
+def add(a, b):
+    "Add two numbers together"
+    total = a+b
+    return total
+
+
+def mul(a, b):
+    "Multiply two numbers together"
+    product = a*b
+    return product
+
+
+def div(a, b):
+    "Divide a number by another number"
+    ratio = a/b
+    return ratio
