686fbaefde68918a89a2c2dc713a94ddc6f2edde
David Mertz <mertz@gnosis.cx>
2022-12-01 11:07:16 -0500

    sdt: tag release v1.0.0

---
+++ pkg/types/types_test.go
@@ -0,0 +1,7 @@
+package types_test
+
+/* This package only contains some struct defs, some enums, some values of 
+   those structs, and a mapping of strings to a struct type.  I.e. no funcs. 
+   Everything availble to test is alredy handled by the compiler and other
+   packages that utilize these items
+*/
